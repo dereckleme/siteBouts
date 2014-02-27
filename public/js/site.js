44898a068f7ab7c385ee65d13097ad3edef483e4
root <root@dereck>
2014-02-27 18:50:14 -0300

    Change-Id: I5f63aab9c3c788e5e98b46e38b87b014cebdf671

--- public/js/site.js
+++ public/js/site.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
 	$(".nav-button").click(function () {
 		$(".nav-button,.box-menu").toggleClass("open");
 	});
-
+	var w = window.innerWidth;
+	if(w <= 800)
+	{
+		$(".g-produtos").css('overflow-x', 'auto');
+	}
+	window.onresize = function(event) {
+		if(w <= 800)
+			{
+				$(".g-produtos").css('overflow-x', 'auto');
+			}
+	};
 });
