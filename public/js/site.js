e0edbf63c3623d822b48c2bacabeb5cb2831ba6c
Administrador <Administrador@Dereck>
2014-03-18 14:13:10 -0300

    slide

--- public/js/site.js
+++ public/js/site.js
@@ -13,4 +13,15 @@ $(document).ready(function(){
 				$(".g-produtos").css('overflow-x', 'auto');
 			}
 	};
+	$( window ).scroll(function() {
+			var posicao = $(document).scrollTop();
+			if(posicao >= 58)
+				{
+					$("#wowslider-container1").css("top",(posicao-58)+"px");
+				}
+			else
+				{
+					$("#wowslider-container1").css("top","0px");
+				}
+		});
 });
