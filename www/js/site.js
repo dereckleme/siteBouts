3aa4b4fb464d36d3370ae0cdbb20e99406fdeed9
Administrador <Administrador@Dereck>
2014-07-30 17:24:50 -0300

    dsadsa

--- www/js/site.js
+++ www/js/site.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 		zoomWindowFadeOut: 500, 
 		lensFadeIn: 500, 
 		lensFadeOut: 500,
-		zoomWindowWidth: 100,
-		zoomWindowHeight: 100
+		zoomWindowWidth: 390,
+		zoomWindowHeight: 392
 	});
 	//   == se for igualx
 	//   != se não for igual
